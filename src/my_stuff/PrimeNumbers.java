91dabb2922d6027e28c00f77f48b9658870cb766
marph625 <marvin.gnesner@gmail.com>
2024-04-16 13:40:25 +0200

    fixed simple prime numbers program

--- src/my_stuff/PrimeNumbers.java
+++ src/my_stuff/PrimeNumbers.java
@@ -1,20 +1,27 @@
-package Experiment1;
+package my_stuff;
 import Prog1Tools.IOTools;
 
 public class PrimeNumbers {
 
 	public static void main(String[] args) {
-		
-		
+
 		System.out.println("Alle Primzahlen zwischen 2 und einer eingegebenen Zahl sollen berechnet werden.");
-		int primes = IOTools.readInt("Gib eine Zahl ein\n >> ");
-		
-		for (int i = 2; i < primes; i++) {
-			if (i % i != 0 && i % 1 != 0) {
-				System.out.println("Keine Primzahl");
-			} else {
-				System.out.println(i);
+		int num = IOTools.readInt("Gib eine Zahl ein\n >> ");
+		boolean flag = false;
+
+		if (num == 1) {
+			System.out.println(num + " is not a prime number");
+		} else if (num > 1) {
+			for (int i = 2; i < num; i++) {
+				if (num % i == 0) {
+					flag = true;
+					break;
+				}
 			}
+			if (flag) {
+				System.out.println(num + " is not a prime number");
+			} else
+				System.out.println(num + " is a prime number!!!");
 		}
 	}
 }
